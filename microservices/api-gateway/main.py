20f8d11e2adb66c1b00f35786209d83741c2c873
ibrahim <abomokh@mail.tau.ac.il>
2025-05-11 11:47:24 +0300

    added drafts service router

--- microservices/api-gateway/main.py
+++ microservices/api-gateway/main.py
@@ -1,11 +1,12 @@
 # main.py
 from fastapi import FastAPI
-from routers import ai  # import our ai routes
+from routers import ai, drafts, users, emails
 
 app = FastAPI()
 
 # Register routes
 app.include_router(ai.router, prefix="/ai")
+app.include_router(drafts.router, prefix="/drafts")
 
 @app.get("/")
 def root():
