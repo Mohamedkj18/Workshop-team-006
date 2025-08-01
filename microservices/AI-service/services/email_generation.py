c0bd6b8bb090d411245cc2b778535aad5e44a3a7
Mohamedkj18 <mohamedj@mail.tau.ac.il>
2025-08-01 12:30:33 +0300

    Commit

--- microservices/AI-service/services/email_generation.py
+++ microservices/AI-service/services/email_generation.py
@@ -7,7 +7,7 @@ def generate_email(prompt: str) -> str:
     if not validate_prompt(prompt):
         return "Invalid prompt. Please try again."
     gptPrompt = style_prompt = f"""
-        You are writing on behalf of lazy mail. Their writing style is friendly, moderately complex, and detailed. 
+        You are writing on behalf of lazy mail user. Their writing style is friendly, moderately complex, and detailed. 
         They prefer an informal tone and tend to ask questions often, reflecting an inquisitive processing style. 
         Their sentences average around 15 words, they rarely use passive voice, and their emails are highly subjective 
         with a generally positive sentiment. The ideal reading grade level is around 6.5.
